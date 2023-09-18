89a1e140db47e8a8088197623c151e3194116714
ibrahim alnakeeb <ibrahim.nakeeb.2001@gmail.com>
2023-09-18 14:46:03 +0300

    Implement Assign class

---
+++ sources/statements/Assign.cpp
@@ -0,0 +1,29 @@
+#include "../../headers/statements/Assign.hpp"
+
+Assign::Assign(string v, Expression* e) : variable(v), expression(e) {}
+
+Assign::~Assign() {
+	delete expression;
+}
+
+
+void Assign::execute() {
+	
+	Type symbolType = symbolTable.getSymbolType(variable);
+	Term term = expression->evaluate();
+	if (symbolType != term.getType())
+		throw runtime_error("Symbol type mismatch");
+	symbolTable.setSymbolValue(variable, term.getValue(), term.getType());
+
+}
+
+void Assign::print(int level, ofstream& out) {
+	printDepth(level, out);
+	out << "new AssignStatement(" << endl;
+	printDepth(level + 1, out);
+	out << "\"" << variable << "\"," << endl;
+	expression->print(level + 1, out);
+	out << endl;
+	printDepth(level, out);
+	out << ")";
+}
