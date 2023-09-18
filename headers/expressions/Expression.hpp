06fb16e520eef46b52390cc417ce4a424b1fa8b5
ibrahim alnakeeb <ibrahim.nakeeb.2001@gmail.com>
2023-09-18 12:53:58 +0300

    Set value method

---
+++ headers/expressions/Expression.hpp
@@ -0,0 +1,27 @@
+#pragma once
+
+#ifndef EXPRESSION_H
+#define EXPRESSION_H
+
+#include <vector>
+#include <stdexcept>
+#include "../Term/Term.hpp"
+
+enum class Action {
+	plus,
+	minus,
+	concat,
+	index
+};
+
+class Expression {
+
+public:
+	virtual ~Expression();
+	virtual Term evaluate() = 0;
+	virtual void print(int level, ofstream& out) = 0;
+
+};
+
+
+#endif // EXPRESSION_H
