810e98dac3f36a1307edf101e0def5e1d4edbfd7
ibrahim alnakeeb <ibrahim.nakeeb.2001@gmail.com>
2023-09-18 13:02:21 +0300

    Add UnaryExpression class

---
+++ headers/expressions/UnaryExpression.hpp
@@ -0,0 +1,21 @@
+#pragma once
+
+#ifndef UNARY_EXPRESSION_H
+#define UNARY_EXPRESSION_H
+
+#include "Expression.hpp"
+
+
+class UnaryExpression : public Expression {
+
+	Term* Term;
+
+public:
+	UnaryExpression(Term* l = nullptr);
+	~UnaryExpression();
+	
+	Term evaluate() override;
+	void print(int level, ofstream& out) override;
+};
+
+#endif // UNARY_EXPRESSION_H
